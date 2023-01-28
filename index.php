cb426fcf4b8f02b6e2cf8e4df7636e5714e095b6
playtvdxb <85004014+playtvdxb@users.noreply.github.com>
2023-01-28 14:34:37 +0400

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <h1><?php
 $command = escapeshellcmd('python test.py');
 $output = shell_exec($command);
-echo "ARIVARASAN SUN TV";
+echo "SUN TV";
 ?>
 <h1>
 <iframe src="<?=$output?>"width="100%" height="100%"></iframe>
